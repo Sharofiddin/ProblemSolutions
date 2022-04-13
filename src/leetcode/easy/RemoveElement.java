347770a587873cdc90af7f97d0e8865df5b75126
sharofiddin <pardayev.sharofiddin@gmail.com>
2022-04-13 12:05:54 +0500

    matrix

--- src/leetcode/easy/RemoveElement.java
+++ src/leetcode/easy/RemoveElement.java
@@ -2,27 +2,28 @@ package leetcode.easy;
 
 import java.util.Arrays;
 
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int res = nums.length;
-        Arrays.sort(nums);
-        while(res != 0 && nums[res-1] == val) {
-        	res--;
-        }
-        if(res == 0) return 0;
-    	for (int i = 0; i < res; i++) {
-			if(nums[i] == val) {
-				if(val == nums[res-1])
-					return i;
-				nums[i] = nums[res-1];
-				nums[res-1] = val;
+public class RemoveElement {
+	class Solution {
+		public int removeElement(int[] nums, int val) {
+			int res = nums.length;
+			Arrays.sort(nums);
+			while(res != 0 && nums[res-1] == val) {
 				res--;
 			}
+			if(res == 0) return 0;
+			for (int i = 0; i < res; i++) {
+				if(nums[i] == val) {
+					if(val == nums[res-1])
+						return i;
+					nums[i] = nums[res-1];
+					nums[res-1] = val;
+					res--;
+				}
+			}
+			return res;
 		}
-    	return res;
-    }
-}
-public class RemoveElement {
+	}
+	
 	public static void main(String[] args) {
 		int []arr1 = {0,2,2,1,2,1,2,2,4};
 		int []arr2 = {0,1,2,2,3,0,4,2};
---
+++ src/leetcode/easy/SpiralMatrix.java
@@ -0,0 +1,47 @@
+package leetcode.easy;
+
+public class SpiralMatrix{
+
+        public static int[][] generateMatrix(int n) {
+         int[][] ret = new int[n][n];    
+         int i = 0;
+         int right = n-1;
+         int left = 0;
+         int bottom = n-1;
+         int top = 0;
+         while(i < n*n){
+            for(int j = left; j < right; j++){
+                ret[top][j] = ++i;
+            }
+            top++;
+            for(int j = top; j < bottom; j++){
+                ret[j][right] = ++i;
+            }
+            right--;
+            
+    
+            for(int j = right; j >= left; j--){
+                ret[bottom][j] = ++i;
+            }
+            bottom--;
+            for(int j = bottom; j>=top; j--){
+                ret[j][left] = ++i;
+            }
+            right++;
+         }
+         return ret;
+        }
+    public static void main(String[] args) {
+        printMatrix(generateMatrix(5));
+    }
+
+    public static void printMatrix(int[][] mat){
+        for (int i = 0; i <  mat.length;i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.println(mat[i][j]);
+            }
+            System.out.println();
+            
+        }
+    }
+}
