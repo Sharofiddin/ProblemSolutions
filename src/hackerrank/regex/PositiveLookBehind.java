2c811e285d43efd55a8a73866d980105e15b6edd
sharofiddin <pardayev.sharofiddin@gmail.com>
2022-05-05 14:33:47 +0500

    regex assertions

---
+++ src/hackerrank/regex/PositiveLookBehind.java
@@ -0,0 +1,31 @@
+package hackerrank.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PositiveLookBehind {    
+    public static class Regex_Test {
+
+	    public static void checker(String Regex_Pattern){
+	    
+	        Scanner input = new Scanner(System.in);
+	        String Test_String = input.nextLine();
+	        Pattern p = Pattern.compile(Regex_Pattern);
+	        Matcher m = p.matcher(Test_String);
+	        int Count = 0;
+	        while(m.find()){
+	            Count += 1;
+	        }
+	        System.out.format("Number of matches : %d",Count);
+	        input.close();
+	    }   
+    public static void main(String[] args) {
+         Regex_Test.checker("(?<=[13579])[0-9]"); //Use '\\' instead of '\'.
+    
+    }
+}
+
+
+    
+}
